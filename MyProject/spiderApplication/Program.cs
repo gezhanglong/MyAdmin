c6981dc571eb87fe1fe0f68820b52934d7b3acb2
gezhanglong <38719428+gezhanglong@users.noreply.github.com>
2018-11-01 18:07:43 +0800

    20181101

--- MyProject/spiderApplication/Program.cs
+++ MyProject/spiderApplication/Program.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 using DotnetSpider.Core;
 using DotnetSpider.Core.Pipeline;
 using DotnetSpider.Core.Processor;
-using DotnetSpider.Extraction;
-using System.Security.Policy;
 using DotnetSpider.Core.Scheduler;
-using DotnetSpider.Downloader;
+using DotnetSpider.Core.Selector;
+using DotnetSpider.Extension;
+using DotnetSpider.Core.Downloader;
+using DotnetSpider.Extension.Model;
+using DotnetSpider.Extension.Model.Attribute;
+using DotnetSpider.Extension.Pipeline;
 
 namespace spiderApplication
 {
@@ -18,51 +21,85 @@ namespace spiderApplication
     {
         static void Main(string[] args)
         {
-            var site = new Site { CycleRetryTimes = 3, SleepTime = 300 };
-            var spider = Spider.Create(site, new GithubProfileProcessor()).AddStartUrl("https://github.com/zlzforever");
-            spider.ThreadNum = 5;
-            spider.Run();
-            Console.Read();
+            CustmizeProcessorAndPipeline();
             Console.WriteLine("Press any key to continue...");
             Console.Read();
         }
+
+
+        public static void CustmizeProcessorAndPipeline()
+        {
+            // Config encoding, header, cookie, proxy etc... 定义采集的 Site 对象, 设置 Header、Cookie、代理等
+            var site = new Site { EncodingName = "UTF-8", RemoveOutboundLinks = true ,Domains= "www.cnblogs.com".Split(';') };
+            //for (int i = 1; i < 5; ++i)
+            //{
+                // Add start/feed urls. 添加初始采集链接
+                site.AddStartUrl($"https://www.cnblogs.com/post/prevnext?postId=5525467&blogId=43926&dateCreated=2016%2F5%2F24+23%3A47%3A00&postType=1");
+            //}
+            Spider spider = Spider.Create(site,
+                // use memoery queue scheduler. 使用内存调度
+                new QueueDuplicateRemovedScheduler(),
+                // use custmize processor for youku 为优酷自定义的 Processor
+                new YoukuPageProcessor())
+                // use custmize pipeline for youku 为优酷自定义的 Pipeline
+                .AddPipeline(new YoukuPipeline());
+            spider.Downloader = new HttpClientDownloader();
+            spider.ThreadNum = 1;
+            spider.EmptySleepTime = 3000;
+           // spider.Deep = 4;
+            // Start crawler 启动爬虫
+            spider.Run();
+
+        }
     }
 
-    public class MyPageProcessor : BasePageProcessor
+    public class YoukuPipeline : BasePipeline
+    {
+        private static long count = 0;
+         
+        public override void Process(IEnumerable<ResultItems> resultItems, ISpider spider)
+        {
+            foreach (var resultItem in resultItems)
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach (YoukuVideo entry in resultItem.Results["VideoResult"])
+                {
+                    count++;
+                    builder.Append($" [YoukuVideo {count}] {entry.Name}");
+                }
+                Console.WriteLine(builder);
+            }
+        }
+    }
+
+    public class YoukuPageProcessor : BasePageProcessor
     {
         protected override void Handle(Page page)
         {
-            // 利用 Selectable 查询并构造自己想要的数据对象   
-            var totalVideoElements = page.Selectable().SelectList(Selectors.XPath("//div[@class='yk-pack pack-film']")).Nodes();
-            List<DataModel> results = new List<DataModel>();
+            // 利用 Selectable 查询并构造自己想要的数据对象 
+            var totalVideoElements = page.Selectable.SelectList(Selectors.XPath("*")).Nodes();
+            List<YoukuVideo> results = new List<YoukuVideo>();
             foreach (var videoElement in totalVideoElements)
             {
-                var video = new DataModel();
-                video.Name = videoElement.Select(Selectors.XPath(".//img[@class='quic']/@alt")).GetValue();
+                var video = new YoukuVideo();
+                video.Name = videoElement.Select(Selectors.XPath("//a[@title]")).GetValue();
                 results.Add(video);
             }
 
             // Save data object by key. 以自定义KEY存入page对象中供Pipeline调用
             page.AddResultItem("VideoResult", results);
-        }
-         
-    }
 
-    public class MyPipeline : BasePipeline
-    {
-        private static long count = 0; 
-
-        public override void Process(IList<ResultItems> resultItems,  dynamic sender = null)
-        {
-            StringBuilder builder = new StringBuilder();
-            foreach (ResultItems entry in resultItems)
+            // Add target requests to scheduler. 解析需要采集的URL
+            foreach (var url in page.Selectable.SelectList(Selectors.XPath("//ul[@class='yk-pages']")).Links().Nodes())
             {
-                count++;
-                builder.Append($" [YoukuVideo {count}] {entry.Values}");
-            }
-            Console.WriteLine(builder);
-
-            // Other actions like save data to DB. 可以自由实现插入数据库或保存到文件
+                page.AddTargetRequest(new Request(url.GetValue(), null));
+            } 
         }
     }
+
+    public class YoukuVideo
+    {
+        public string Name { get; set; }
+    }
+
 }
