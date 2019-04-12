fdf13d01c964e9d916f3f03ff3d805f67c5ccd86
gezhanglong <38719428+gezhanglong@users.noreply.github.com>
2019-04-12 18:02:53 +0800

    2019-4-12

--- 项目2/miniprogram/pages/csstest5/csstest5.js
+++ 项目2/miniprogram/pages/csstest5/csstest5.js
@@ -1,12 +1,10 @@
-// miniprogram/pages/csstest4/csstest4.js
+// miniprogram/pages/csstest5/csstest5.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    windowWidth: 0,//当前屏幕宽度
-    windowHeight: 0,//当前屏幕高度 
     indicatorDots: true,//是否显示面板指示点
     autoplay: false,//是否自动切换
     circular: true,//是否采用衔接滑动
@@ -15,41 +13,51 @@ Page({
     duration: 600,//滑动动画时长
     previousMargin: '0',//前边距，可用于露出前一项的一小部分
     nextMargin: '0',//后边距，可用于露出后一项的一小部分
-    seat:[],//星星位置
-  },
-
-  // onanimationfinish:function(e){
-  //   console.log("onanimationfinish:"+JSON.stringify(e))
-  // },
-
-  // ontransition: function (e) {
-  //   console.log("ontransition:" + JSON.stringify(e))
-  // },
-
-  //swiper滑动事件
-  onchange: function (e) {
-    console.log("onchange:" + JSON.stringify(e))
-    console.log("onchange:" + e.detail.current)
+    
+    windowWidth: 0,//当前屏幕宽度
+    windowHeight: 0,//当前屏幕高度  
+    seat: [],//星星位置
+    poptop:'100%',//弹窗top值
+    poptext:'open',//弹窗文本
   },
 
   //产生星星位置
-  onstar: function () { 
-    var that=this;
+  onstar: function () {
+    var that = this;
     var starclass = ['star', 'star1', 'star2', 'star3'];
-    for(var i=0;i<100;i++){
-      var width = Math.floor(Math.random() * (that.data.windowWidth*2.3));
-      var height = Math.floor(Math.random() * (that.data.windowHeight*2.3));
-      var classid=Math.floor(Math.random()*4);
-      var animationdelay = Math.floor(Math.random()*20);
-      var animationtime = Math.floor(Math.random() * 6); 
-      var newarray = { starclass: starclass[classid], top: height, left: width, animation: "animation: animation_star " + animationtime +"s ease-in-out " + animationdelay+"s infinite;"  };
+    for (var i = 0; i < 60; i++) {
+      var width = Math.floor(Math.random() * (that.data.windowWidth * 2.3));
+      var height = Math.floor(Math.random() * (that.data.windowHeight * 2.3));
+      var classid = Math.floor(Math.random() * 4);
+      var animationdelay = Math.floor(Math.random() * 20);
+      var animationtime = Math.floor(Math.random() * 6);
+      var newarray = { starclass: starclass[classid], top: height, left: width, animation: "animation: animation_star " + animationtime + "s ease-in-out " + animationdelay + "s infinite;" };
       that.setData({
-        seat: that.data.seat.concat(newarray),   
-      }); 
-    } 
+        seat: that.data.seat.concat(newarray),
+      });
+    }
     console.log("seat：" + JSON.stringify(that.data.seat));
   },
 
+  //弹窗事件
+  onopen:function(){
+    var that = this;
+    if(that.data.poptop=='100%'){
+      that.setData({
+        poptop: '45%',
+        poptext:'close'
+      });
+    }else{
+      that.setData({
+        poptop: '100%',
+        poptext: 'open'
+      });
+    }
+
+  },
+
+  //禁止页面滑动
+  onCatchtouchmove:function(){},
 
 
   /**
