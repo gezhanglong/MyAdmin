1c1d5f84db2166a1463f8d8df86506e03d80f863
gezhanglong <38719428+gezhanglong@users.noreply.github.com>
2019-04-17 18:06:04 +0800

    2019-4-17

--- 项目2/miniprogram/pages/csstest7/csstest7.js
+++ 项目2/miniprogram/pages/csstest7/csstest7.js
@@ -119,6 +119,8 @@ Page({
    */
   onLoad: function (options) {
     var that = this;
+    var i=200%100;
+    console.log("i:"+i)
     wx.getSystemInfo({//获取系统信息方法
       success: function (res) {
         that.setData({
@@ -135,7 +137,24 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    const context = wx.createCanvasContext('mycanvas')
 
+    context.setStrokeStyle('#00ff00')
+    context.setLineWidth(5)
+    context.rect(0, 0, 200, 200)
+    context.stroke()
+    context.setStrokeStyle('#ff0000')
+    context.setLineWidth(2)
+    context.moveTo(160, 100)
+    context.arc(100, 100, 60, 0, 2 * Math.PI, true)
+    context.moveTo(140, 100)
+    context.arc(100, 100, 40, 0, Math.PI, false)
+    context.moveTo(85, 80)
+    context.arc(80, 80, 5, 0, 2 * Math.PI, true)
+    context.moveTo(125, 80)
+    context.arc(120, 80, 5, 0, 2 * Math.PI, true)
+    context.stroke()
+    context.draw()
   },
 
   /**
